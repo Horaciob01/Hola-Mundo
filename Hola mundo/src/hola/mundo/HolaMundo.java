c815e88ba98f1662603421c53a2a6e61a3f71e2d
Casa <Casa@Casa-PC>
2016-07-23 18:27:50 -0600

    1 version

---
+++ Hola mundo/src/hola/mundo/HolaMundo.java
@@ -0,0 +1,16 @@
+package hola.mundo;
+
+/**
+ *
+ * @author Casa
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.printf("Hola mundo\n");
+    }
+    
+}
